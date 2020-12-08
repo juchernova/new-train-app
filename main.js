c4196faa6a19967ae8c2067acd1617c34a4180a8
Alex <34835812+alexbliznec@users.noreply.github.com>
2020-12-08 14:59:04 +0300

    made editing and formatting

--- main.js
+++ main.js
@@ -1,100 +1,80 @@
 // testing
-    $(document).ready(function(){
-        $('.phone-number').mask('+7(000) 000-00-00');
-        });
-    
+let inputEmail = document.getElementById("userEmail");
+let pass = document.getElementById("userPassword");
+let passConfirm = document.getElementById("userPasswordConfirm");
+let DisplayPass = document.getElementById("showHidden");
+let DisplayPassConfirm = document.getElementById("showHiddenConfirm");
+let inputNameValue, inputEmailValue, inputPhoneValue, inputPasswordValue;
+let result = document.getElementById("result");
+let formContent = document.getElementById("formContent");
+let emailValid = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/;
 
-    let inputEmail = document.getElementById('inputEmail');
-    let pass = document.getElementById('inputPassword');
-    let passConfirm = document.getElementById('inputPasswordConfirm');
-    let showHidden = document.getElementById('showHidden');
-    let showHiddenConfirm = document.getElementById('showHiddenConfirm');
-    let inputNameValue, inputEmailValue, inputPhoneValue, inputPasswordValue;
-    let result = document.getElementById('result');
-    let formContent = document.getElementById('formContent');
-    
-    let setListener = (element, type, hendler) => {
-        if (!element){
-        return;
-    }
-    element.addEventListener(type, hendler);
-    return () => {
-        element.removeEventListener(type, hendler);
+let setListener = (element, type, hendler) => {
+  if (!element) {
+    return;
+  }
+  element.addEventListener(type, hendler);
+  return () => {
+    element.removeEventListener(type, hendler);
+  };
+};
+$(document).ready(function () {
+  $(".phone-number").mask("+7(000) 000-00-00");
+});
 
-    }
+function check() {
+  if (pass.value !== passConfirm.value) {
+    (pass, passConfirm).style.color = "red";
+  } else {
+    (pass, passConfirm).style.color = "black";
+  }
 }
- 
-    function check() {
-        if (pass.value !== passConfirm.value) {
-            (pass, passConfirm).style.color = "red"
-        } else {
-            (pass, passConfirm).style.color = "black"
-        }
-    };
-    
-    function showPassword(){
-           
-        if (pass.getAttribute('type') == 'password') {
-            showHidden.classList.add('view');
-            pass.setAttribute('type', 'text');
-        } else {
-            showHidden.classList.remove('view');
-            pass.setAttribute('type', 'password');
-        }
-        return false;
-    };
-    
-    function showPasswordConfirm(){
-           
-        if (passConfirm.getAttribute('type') == 'password') {
-            showHiddenConfirm.classList.add('view');
-            passConfirm.setAttribute('type', 'text');
-        } else {
-            showHiddenConfirm.classList.remove('view');
-            passConfirm.setAttribute('type', 'password');
-        }
-        return false;
-    };
 
-    setListener(passConfirm,"keyup", check);
-   setListener(showHidden,"click", showPassword);
-   setListener(showHiddenConfirm,"click", showPasswordConfirm);
-   
-    let emailValid = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/;
-    
-    setListener(inputEmail,'keyup', () => {
-        if (emailValid.test(inputEmail.value)) {
-            inputEmail.style.borderBottomColor = '#007d7c';
-            
-        } else {
-            inputEmail.style.borderBottomColor = 'red';
-        }
-    }
-    ); 
+function showPassword() {
+  if (pass.getAttribute("type") == "password") {
+    DisplayPass.classList.add("view");
+    pass.setAttribute("type", "text");
+  } else {
+    DisplayPass.classList.remove("view");
+    pass.setAttribute("type", "password");
+  }
+  return false;
+}
 
-   
-    
-    function resultGo() {
-    
-    inputNameValue = document.getElementById('inputName').value;
-    inputEmailValue = document.getElementById('inputEmail').value;
-    inputPhoneValue = document.getElementById('inputPhone').value;
-    inputPasswordValue = inputPassword.value;
-    formContent.style.display = 'none';
-        document.getElementById('helloUser').innerHTML = 'Добро пожаловать!';
-        document.getElementById('youName').innerHTML = inputNameValue;
-        document.getElementById('youEmail').innerHTML = inputEmailValue;
-        document.getElementById('youPhone').innerHTML = inputPhoneValue;
-        document.getElementById('youPass').innerHTML = inputPasswordValue;
-    }
-    
-    result.addEventListener( "click", resultGo);
+function showPasswordConfirm() {
+  if (passConfirm.getAttribute("type") == "password") {
+    DisplayPassConfirm.classList.add("view");
+    passConfirm.setAttribute("type", "text");
+  } else {
+    DisplayPassConfirm.classList.remove("view");
+    passConfirm.setAttribute("type", "password");
+  }
+  return false;
+}
 
-   
-    
+setListener(passConfirm, "keyup", check);
+setListener(DisplayPass, "click", showPassword);
+setListener(DisplayPassConfirm, "click", showPasswordConfirm);
 
-     
-       
-    
-    
-    
+setListener(inputEmail, "keyup", () => {
+  if (emailValid.test(inputEmail.value)) {
+    inputEmail.style.borderBottomColor = "#007d7c";
+  } else {
+    inputEmail.style.borderBottomColor = "red";
+  }
+});
+
+function resultOutput() {
+  inputNameValue = document.getElementById("userName").value;
+  inputEmailValue = document.getElementById("userEmail").value;
+  inputPhoneValue = document.getElementById("userPhone").value;
+  inputPasswordValue = document.getElementById('userPassword').value;
+  formContent.style.display = "none";
+  document.getElementById("helloUser").innerHTML = "Добро пожаловать!";
+  document.getElementById("youName").innerHTML = inputNameValue;
+  document.getElementById("youEmail").innerHTML = inputEmailValue;
+  document.getElementById("youPhone").innerHTML = inputPhoneValue;
+  document.getElementById("youPass").innerHTML = inputPasswordValue;
+}
+
+setListener(result, "click", resultOutput);
