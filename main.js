185f945b7f886b1f771bbb8cf6f8505fe314ef69
Alex <34835812+alexbliznec@users.noreply.github.com>
2020-12-09 15:35:25 +0300

    fixed code reveiw comments

--- main.js
+++ main.js
@@ -1,80 +1,68 @@
-// testing
-let inputEmail = document.getElementById("userEmail");
-let pass = document.getElementById("userPassword");
-let passConfirm = document.getElementById("userPasswordConfirm");
-let DisplayPass = document.getElementById("showHidden");
-let DisplayPassConfirm = document.getElementById("showHiddenConfirm");
-let inputNameValue, inputEmailValue, inputPhoneValue, inputPasswordValue;
-let result = document.getElementById("result");
-let formContent = document.getElementById("formContent");
-let emailValid = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/;
+const userEmailInput = document.getElementById('userEmail');
+const pass = document.getElementById('userPassword');
+const passConfirm = document.getElementById('userPasswordConfirm');
+const DisplayPass = document.getElementById('showHidden');
+const DisplayPassConfirm = document.getElementById('showHiddenConfirm');
+const getUserData = document.getElementById('getUserData');
+const formContent = document.getElementById('formContent');
+const capitalSnakeCase = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/;
 
-let setListener = (element, type, hendler) => {
-  if (!element) {
-    return;
+const setListener = (element, type, handler) => {
+  if (element) {
+    element.addEventListener(type, handler);
   }
-  element.addEventListener(type, hendler);
-  return () => {
-    element.removeEventListener(type, hendler);
-  };
 };
+
 $(document).ready(function () {
-  $(".phone-number").mask("+7(000) 000-00-00");
+  $('.phone-number').mask('+7(000) 000-00-00');
 });
 
-function check() {
-  if (pass.value !== passConfirm.value) {
-    (pass, passConfirm).style.color = "red";
-  } else {
-    (pass, passConfirm).style.color = "black";
-  }
-}
+function checkPassword() {
+  pass.value !== passConfirm.value
+    ? ((pass, passConfirm).style.color = 'red')
+    : ((pass, passConfirm).style.color = 'black');
+};
 
 function showPassword() {
-  if (pass.getAttribute("type") == "password") {
-    DisplayPass.classList.add("view");
-    pass.setAttribute("type", "text");
+  if (pass.getAttribute('type') === 'password') {
+    DisplayPass.classList.add('view');
+    pass.setAttribute('type', 'text');
   } else {
-    DisplayPass.classList.remove("view");
-    pass.setAttribute("type", "password");
+    DisplayPass.classList.remove('view');
+    pass.setAttribute('type', 'password');
   }
   return false;
-}
+};
 
 function showPasswordConfirm() {
-  if (passConfirm.getAttribute("type") == "password") {
-    DisplayPassConfirm.classList.add("view");
-    passConfirm.setAttribute("type", "text");
+  if (passConfirm.getAttribute('type') === 'password') {
+    DisplayPassConfirm.classList.add('view');
+    passConfirm.setAttribute('type', 'text');
   } else {
-    DisplayPassConfirm.classList.remove("view");
-    passConfirm.setAttribute("type", "password");
+    DisplayPassConfirm.classList.remove('view');
+    passConfirm.setAttribute('type', 'password');
   }
   return false;
-}
+};
 
-setListener(passConfirm, "keyup", check);
-setListener(DisplayPass, "click", showPassword);
-setListener(DisplayPassConfirm, "click", showPasswordConfirm);
+function checkEmail() {
+  !capitalSnakeCase.test(userEmailInput.value)
+    ? (userEmailInput.style.borderBottomColor = 'red')
+    : (userEmailInput.style.borderBottomColor = '#007d7c');
+};
 
-setListener(inputEmail, "keyup", () => {
-  if (emailValid.test(inputEmail.value)) {
-    inputEmail.style.borderBottomColor = "#007d7c";
-  } else {
-    inputEmail.style.borderBottomColor = "red";
-  }
-});
+setListener(passConfirm, 'keyup', checkPassword);
+setListener(DisplayPass, 'click', showPassword);
+setListener(DisplayPassConfirm, 'click', showPasswordConfirm);
+setListener(userEmailInput, 'keyup', checkEmail);
 
 function resultOutput() {
-  inputNameValue = document.getElementById("userName").value;
-  inputEmailValue = document.getElementById("userEmail").value;
-  inputPhoneValue = document.getElementById("userPhone").value;
-  inputPasswordValue = document.getElementById('userPassword').value;
-  formContent.style.display = "none";
-  document.getElementById("helloUser").innerHTML = "Добро пожаловать!";
-  document.getElementById("youName").innerHTML = inputNameValue;
-  document.getElementById("youEmail").innerHTML = inputEmailValue;
-  document.getElementById("youPhone").innerHTML = inputPhoneValue;
-  document.getElementById("youPass").innerHTML = inputPasswordValue;
-}
+  formContent.style.display = 'none';
+  document.getElementById('helloUser').innerHTML = 'Добро пожаловать!';
+  document.getElementById('yourName').innerHTML = document.getElementById('userName').value;
+  document.getElementById('yourEmail').innerHTML = document.getElementById('userEmail').value;
+  document.getElementById('yourPhone').innerHTML = document.getElementById('userPhone').value;
+  document.getElementById('yourPass').innerHTML = document.getElementById('userPassword').value;
+};
 
-setListener(result, "click", resultOutput);
+setListener(getUserData, 'click', resultOutput);
